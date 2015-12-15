0a44afc64231565d13f98b86dc15eec75c6f4c57
Cordell Wagendorf <dorf06@gmail.com>
2015-12-15 14:34:50 -0600

    Second half completed

--- Day 14/Day 14/Program.cs
+++ Day 14/Day 14/Program.cs
@@ -3,11 +3,32 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Day_14
 {
-    class Reindeer
+    class Score
+    {
+        // Variables
+        public String Name { get; }
+        public int Value { get; set; }
+
+        // Constructor
+        public Score (String name)
+        {
+            Name = name;
+
+            Value = 0;
+        }
+
+        // Compares values
+        public static int CompareTo (Score a, Score b)
+        {
+            return -a.Value.CompareTo(b.Value);
+        }
+    }
+    class Reindeer : IComparable<Reindeer>
     {
         // Stats
         // Name
@@ -46,7 +67,7 @@ namespace Day_14
             RestMarker = 0;
             Stamina = MaxStamina;
         }
-
+        
         // Update method
         public void Update (int SimTime)
         {
@@ -78,6 +99,32 @@ namespace Day_14
                 Distance += Speed;
             }
         }
+
+        public override String ToString()
+        {
+            // "Name: {0}\t Resting: {1}\t Distance: {2}\t Rest Marker: {3}\t", current.Name, current.Resting, current.Distance, current.RestMarker
+            String output = "Name: " + Name + "\t";
+
+            if (!Resting)
+                output += "Stamina:" + Stamina + "\t\t";
+            else
+            {
+                if (RestMarker < 100)
+                    output += "Rest Marker: " + RestMarker + "\t\t";
+                else
+                    output += "Rest Marker: " + RestMarker + "\t";
+            }
+
+            output += "Distance: " + Distance;
+
+            return output;
+        }
+
+        public int CompareTo(Reindeer other)
+        {
+            // Sorts by Distance, highest first
+            return -Distance.CompareTo(other.Distance);
+        }
     }
 
     class Program
@@ -86,17 +133,18 @@ namespace Day_14
         {
             // Status
             Console.WriteLine("Listing racers...");
+            Console.WriteLine("");
 
             // Print out reindeer and stats
             foreach (Reindeer current in input)
             {
-                Console.WriteLine("{0} Speed:{1} Stamina:{2} Rest Time:{3}", current.Name, current.Speed, current.Stamina, current.RestTime);
+                Console.WriteLine("{0}\t Speed:{1}\t Stamina:{2}\t Rest Time:{3}", current.Name, current.Speed, current.Stamina, current.RestTime);
             }
 
-            Console.WriteLine("Done");
+            Console.WriteLine("");
         }
 
-        static void RaceStatus (List<Reindeer> racers, int elapsedTime)
+        static void RaceStatus (List<Reindeer> racers, int elapsedTime, List<Score> scoreboard)
         {
             Console.Clear();
 
@@ -105,8 +153,51 @@ namespace Day_14
 
             foreach (Reindeer current in racers)
             {
-                Console.WriteLine("Name: {0}\t Resting: {1}\t Distance: {2}\t Rest Marker: {3}\t", current.Name, current.Resting, current.Distance, current.RestMarker); 
+                Console.WriteLine(current.ToString()); 
             }
+
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine("Name \t Score");
+            
+            foreach (var item in scoreboard)
+            {
+                Console.WriteLine(item.Name + "\t" + item.Value);
+            }
+        }
+
+        public static void UpdateRace(int raceTime, List<Reindeer> racers, List<Score> scoreboard)
+        {
+            int count = 0;
+
+            // Update racers
+            foreach (Reindeer current in racers)
+            {
+                current.Update(raceTime);
+            }
+
+            // Sort race standings
+            racers.Sort();
+
+            // Update scores
+            // Grab highest distance
+            Reindeer lead = racers[count];
+
+            // Assign point
+            scoreboard.Find(x => x.Name.Equals(lead.Name)).Value++;
+
+            // Inc count to check next reindeer
+            count++;
+
+            // Check if there is a tie
+            while (racers[count].Distance == lead.Distance)
+            {
+                scoreboard.Find(x => x.Name.Equals(racers[count].Name)).Value++;
+
+                count++;
+            }
+
+            scoreboard.Sort(Score.CompareTo);
         }
 
         static Reindeer CreateReindeer (String input)
@@ -126,8 +217,12 @@ namespace Day_14
             String input = null;
             int raceTime = 0;
 
+            // Scoreboard
+            List<Score> scoreBoard = new List<Score>();
+
             // Simulation settings
             const int EndTime = 2503;
+            const int SimulationPace = 100;
 
             // Inititalize collection of reindeer
             List<Reindeer> racers = new List<Reindeer>();
@@ -135,25 +230,41 @@ namespace Day_14
             // Read file input for reindeer
             StreamReader sr = new StreamReader(@"C:\Users\cordell.wagendorf\Documents\GitHubVisualStudio\Advent-of-Code\Day 14\Day 14\input");
 
+            // Create reindeer and add them to race
             while ((input = sr.ReadLine()) != null)
             {
-                racers.Add(CreateReindeer(input));
+                // Create reindeer
+                Reindeer temp = CreateReindeer(input);
+
+                // Add reindeer to race
+                racers.Add(temp);
+
+                // Add reindeer to scoreboard
+                scoreBoard.Add(new Score(temp.Name));
             }
+
+            Console.WriteLine("");
+
+            // Stats
+            ReindeerStats(racers);
+
+            Console.WriteLine("\n\nPress any key to start the race...");
+            Console.ReadLine();
            
             // Simulation loop
             while (raceTime <= EndTime)
             {
                 // Status
-                RaceStatus(racers, raceTime);
+                RaceStatus(racers, raceTime, scoreBoard);
 
-                // Update racers
-                foreach (Reindeer current in racers)
-                {
-                    current.Update(raceTime);
-                }
+                // Update race
+                UpdateRace(raceTime, racers, scoreBoard);
 
                 // Advance clock
                 raceTime++;
+
+                // Simulation Pace
+                Thread.Sleep(SimulationPace);
 
                 // Debug pause
                 //Console.ReadLine();
